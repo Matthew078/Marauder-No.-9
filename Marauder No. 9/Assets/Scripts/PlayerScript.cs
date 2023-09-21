dbe4a51e1c8039b2727dfc42f642686ed48dda17
Tim Lin <lintim03@gmail.com>
2023-09-21 17:48:02 -0500

    Implemented Firing Gun

--- Marauder No. 9/Assets/Scripts/PlayerScript.cs
+++ Marauder No. 9/Assets/Scripts/PlayerScript.cs
@@ -6,13 +6,16 @@ public class PlayerScript : MonoBehaviour
 {
     public float moveForce;
     private float horizontalKey;
+    private bool playerDirection;
 
+    public GameObject gun;
 
     private Rigidbody rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        playerDirection = true;
     }
 
     // Start is called before the first frame update
@@ -26,13 +29,23 @@ public class PlayerScript : MonoBehaviour
     {
         // Player input
         horizontalKey = Input.GetAxis("Horizontal");
+
+        //Firing Gun
+        if (horizontalKey < 0) { playerDirection = false; }
+        else if (horizontalKey > 0) { playerDirection = true; }
+        if (Input.GetButton("Fire1"))
+        {
+            if (gun) 
+            { 
+                gun.GetComponent<GunScript>().FireGun(playerDirection, rb.velocity.x);
+            }
+        }
     }
 
     void FixedUpdate()
     {
-        // Apply force
-        Vector2 force =  new Vector2(horizontalKey, 0f).normalized * moveForce;
+        // Horizontal Force
+        Vector2 force = new Vector2(horizontalKey, 0f).normalized * moveForce;
         rb.AddForce(force);
     }
-
 }
