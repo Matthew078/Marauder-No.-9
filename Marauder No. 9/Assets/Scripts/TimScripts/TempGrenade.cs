6d7508e453313b42564442b75f6458647e2f67e9
Tim Lin <lintim03@gmail.com>
2023-10-13 20:26:37 -0500

    Added Grenade Throw Script

---
+++ Marauder No. 9/Assets/Scripts/TimScripts/TempGrenade.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempGrenade : MonoBehaviour
+{
+    public float explosionForce = 100;
+    public float explosionRadius = 5;
+    public float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public void detonate()
+    {
+        Collider[] cols = Physics.OverlapSphere(this.transform.position, 5f);
+        int i = 0;
+        while (i < cols.Length)
+        {
+            if (cols[i].gameObject.tag == "Enemy")
+            {
+                cols[i].gameObject.GetComponent<Rigidbody>().AddExplosionForce(explosionForce, this.transform.position, explosionRadius);
+                //also subtract health
+            }
+            i++;
+        }
+
+        Destroy(this.gameObject);
+    }
+}
--- Marauder No. 9/Assets/Scripts/TimScripts/TempGrenadeThrow.cs
+++ Marauder No. 9/Assets/Scripts/TimScripts/TempGrenadeThrow.cs
@@ -7,26 +7,62 @@ public class TempGrenadeThrow : MonoBehaviour
     public List<GameObject> grenade_list = new List<GameObject>();
     public GameObject grenade;
     public float throwForce = 100;
-    public float throwDelay;
+    public float throwDelay = 1;
     public float throwTimer;
+    public float grenadeDuration = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        throwTimer = throwDelay + 1;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        timeOutGrenades();
+        if (throwTimer <= throwDelay)
+        {
+            throwTimer += Time.deltaTime;
+        }
+
+        //TEST CODE
+        if (Input.GetKeyDown("f"))
+        {
+            onClick();
+        }
     }
 
+    public void onClick()
+    {
+        if (throwTimer <= throwDelay)
+        {
+            detonateGrenades();
+        }
+        else
+        {
+            throwGrenade();
+            throwTimer = 0;
+        }
+    }
+
+    void timeOutGrenades()
+    {
+        foreach (GameObject clone in new List<GameObject>(grenade_list))
+        {
+            if (clone.GetComponent<TempGrenade>().timer > grenadeDuration)
+            {
+                grenade_list.Remove(clone);
+                clone.GetComponent<TempGrenade>().detonate();
+            }
+        }
+    }
     void detonateGrenades()
     {
         foreach(GameObject clone in new List<GameObject>(grenade_list))
         {
-            //clone.GetComponent<TempGrenade>().detonate();
+            grenade_list.Remove(clone);
+            clone.GetComponent<TempGrenade>().detonate();
         }
     }
 
