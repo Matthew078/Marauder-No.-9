282529160e9f044ad74547ff284994c4245f24f0
Matthew Meldrum <mattmcollege01@gmail.com>
2023-10-21 19:36:37 -0500

    good stuff

---
+++ Marauder No. 9/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField]
+    private NewPlayerScript p;
+    [SerializeField]
+    private float speed = 5f;
+    [Header("Jump Settings")]
+    [SerializeField]
+    private float jumpPower = 10f;
+    [SerializeField]
+    private float jumpMultiplier = 1f;
+    [SerializeField]
+    private float fallMultiplier = 1f;
+
+    private bool canJump;
+
+    private void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        canJump = false;
+    }
+
+    // Update is called once per frame
+    
+    private void Update()
+    {
+        if (p.gc.IsGrounded() && p.pi.inputVertical > 0.1f)
+        {
+            canJump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // movement mechanics
+        Vector3 velocity = new Vector3(p.pi.inputHorizontal * speed, p.rb.velocity.y, 0f);
+        p.rb.velocity = velocity;
+        // jump mechanics       
+        if (canJump)
+        {
+            canJump = false;
+            p.rb.velocity = new Vector3(p.rb.velocity.x, jumpPower, 0f);
+        }
+        if (p.rb.velocity.y < 0)
+        {
+            p.rb.velocity -= new Vector3(0f, -Physics.gravity.y, 0f) * fallMultiplier * Time.deltaTime;
+        }
+    }
+    
+}
---
+++ Marauder No. 9/Assets/Scripts/TimScripts/PlayerInputScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputScript : MonoBehaviour
+{
+    [Header("Input Configurations")]
+    [SerializeField]
+    private string horizontalAxis = "Horizontal";
+    [SerializeField]
+    private string verticalAxis = "Vertical";
+    [SerializeField]
+    private string interactButton = "e";
+
+    public float inputHorizontal { get; private set; }
+    public float inputVertical { get; private set; }
+    public bool inputInteract { get; private set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inputHorizontal = 0;
+        inputVertical = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxis(horizontalAxis);
+        inputVertical = Input.GetAxisRaw(verticalAxis);
+        inputInteract = Input.GetKeyDown(interactButton);
+    }
+}
