47a809202918ea4e355d1c49f8cb38ae7438be38
Jalin J <83527423+Crocospyle@users.noreply.github.com>
2023-12-04 13:11:04 -0600

    Polish

--- Marauder No. 9/Assets/Scripts/MattScripts/PlayerMovementScript.cs
+++ Marauder No. 9/Assets/Scripts/MattScripts/PlayerMovementScript.cs
@@ -17,7 +17,11 @@ public class PlayerMovementScript : MonoBehaviour
     private float jumpMultiplier = 1f;
     [SerializeField]
     private float fallMultiplier = 1f;
+    private bool hover;
     private bool canJump;
+    private float hoverTimer;
+    public ParticleSystem jetfire;
+
 
     // Awake is called when an enabled script instance is being loaded
     private void Awake()
@@ -29,25 +33,48 @@ public class PlayerMovementScript : MonoBehaviour
     {
         canJump = false;
         p.rb.isKinematic = false;
+        hover = false;
+        hoverTimer = 0f;
+        jetfire.Stop();
     }
 
     // Update is called once per frame
     private void Update()
     {
         // Check if player is able to jump
+        print(hoverTimer);
         if (p.gc.IsGrounded() && p.pi.inputVertical > 0.1f)
         {
             canJump = true;
         }
-        // Check if player should freeze when firing weapon
-        if (!p.gc.IsGrounded() && p.pi.inputFire)
+        // Check if player should hover
+        if (!p.gc.IsGrounded() && p.pi.inputHover)
         {
-            p.rb.isKinematic = true;
+            fallMultiplier = -2.5f;
+            hover = true;
+            speed = 2.5f;
+            if (!jetfire.isPlaying)
+            {
+                jetfire.Play();
+            }
         }
         else
         {
-            p.rb.isKinematic = false;
+            fallMultiplier = 2;
+            hover = false;
+            hoverTimer = 0f;
+            speed = 4.3f;
+            jetfire.Stop();
         }
+        if (hover == true)
+        {
+            hoverTimer += Time.deltaTime;
+        }
+        if (hoverTimer >= 2.3f)
+        {
+            hover = false;
+            fallMultiplier = 2;
+        }        
     }
 
     // FixedUpdate is called once per physics frame
